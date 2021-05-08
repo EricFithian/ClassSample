aedbd05958fefe3317926fb2186e0abdf2a85852
Eric Fithian <ef2413@columbia.edu>
2021-05-08 07:19:04 -0700

    Update web.js

--- web.js
+++ web.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 
+var samples = "";
+
+for(let i = 0; i < 20; i++) {
+  samples += "This is example number " + i + " in our index! :)" + <br>;
+}
+
 app.get('/', function (req, res) {
-  res.send('hello Git This Bread!');
+  res.send(samples);
 });
 
 app.listen(process.env.PORT || 3000);
